2f28990dcbf79eaa380bdd1861510693956c20eb
aravind <aravindhv10@gmail.com>
2024-09-22 14:03:45 +0530

    usual emacs update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -140,16 +140,17 @@
   (projectile-mode +1)
   )
 
-(use-package lsp
-  :ensure t
-  :demand t
-  :config
-  (setq gc-cons-threshold 100000000)
-  (setq read-process-output-max 1048576)
-  (setq lsp-idle-delay 0.1)
-  (setq lsp-tcp-connection-timeout 10)
-  :init
-  )
+;; (elpaca lsp)
+;; (use-package lsp
+;;   :ensure t
+;;   :demand t
+;;   :config
+;;   (setq gc-cons-threshold 100000000)
+;;   (setq read-process-output-max 1048576)
+;;   (setq lsp-idle-delay 0.1)
+;;   (setq lsp-tcp-connection-timeout 10)
+;;   :init
+;;   )
 
 (use-package lsp-ui
   :ensure t
@@ -466,8 +467,6 @@
   (key-chord-define-global "\\]" 'evil-window-vsplit)
   (key-chord-define-global "\\[" 'evil-window-vsplit))
 
-;; (elpaca-wait)
-;; (elpaca-try corfu 1)
 (use-package markdown-mode
   :ensure t
   :demand t
@@ -475,24 +474,70 @@
   :init
   )
 
-;; (elpaca-wait)
-;; (elpaca-try corfu 1)
-(use-package company
-  :ensure t
-  :demand t
-  :config
-  (setq company-minimum-prefix-length 0)
-  (setq company-idle-delay 0)
-  (add-hook 'prog-mode-hook 'company-mode)
-  (add-hook 'text-mode-hook 'company-mode)
-  ;; (add-hook 'eshell-mode-hook 'company-mode)
-  ;; :init
-  ;; (global-company-mode)
-  )
+;; ;; (elpaca-wait)
+;; ;; (elpaca-try corfu 1)
+;; (use-package company
+;;   :ensure t
+;;   :demand t
+;;   :config
+;;   (setq company-minimum-prefix-length 0)
+;;   (setq company-idle-delay 0)
+;;   (add-hook 'prog-mode-hook 'company-mode)
+;;   (add-hook 'text-mode-hook 'company-mode)
+;;   ;; (add-hook 'eshell-mode-hook 'company-mode)
+;;   ;; :init
+;;   ;; (global-company-mode)
+;;   )
 
 ;; (elpaca-wait)
-;; (elpaca-try corfu 1)
-;; (use-package corfu)
+;; (elpaca-try corfu)
+(elpaca corfu)
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+
+;; Add extensions
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind
+  ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+  )
 
 (defun myfun/save_and_format_c ()
   (interactive)
