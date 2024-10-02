bbc632b7c47c1e3a68747151244cf252f4e5949f
aravind <aravindhv10@gmail.com>
2024-10-02 14:31:10 +0530

    Added more scripts to run emacs inside flatpak

--- flatpak_emacs/flat_emacs.sh
+++ flatpak_emacs/flat_emacs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo 'export PATH="${HOME}/bin:${PATH}"; /app/bin/emacs-wrapper' | flatpak run --command=sh org.gnu.emacs
+# echo 'export PATH="${HOME}/bin:${PATH}"; /app/bin/emacs-wrapper' | flatpak run --command=sh org.gnu.emacs
+echo 'export PATH="${HOME}/bin:${PATH}"; source /usr/lib/sdk/llvm18/enable.sh; /app/bin/emacs-wrapper' | flatpak run --command=sh org.gnu.emacs
 exit '0'
