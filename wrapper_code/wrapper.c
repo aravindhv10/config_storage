84316ba2b3d2cea297988c3f8550d57636643ec0
aravind <aravindhv10@gmail.com>
2024-10-06 13:46:04 +0530

    more optimizations on the wrapper c code

--- wrapper_code/wrapper.c
+++ wrapper_code/wrapper.c
@@ -3,20 +3,63 @@
 #include <string.h>
 #include <unistd.h>
 
-#define SIZE_BUFFER 1 << 22
+#define SIZE_BUFFER (1 << 22)
+
 static char BUFFER[SIZE_BUFFER];
+static int BUFFER_CURRENT;
+
+static inline char *myalloc(size_t insize) {
+  char *ret = BUFFER + BUFFER_CURRENT;
+  BUFFER_CURRENT += insize;
+  return ret;
+}
 
 static char const proc_self_exe[] = "/proc/self/exe";
 static char const name_ld[] = "/ld-linux-x86-64.so.2";
 static char library_path[] = "--library-path";
 
-char *get_ld(char const *path_dir_prefix) {
+static inline char *get_prefix_dir() {
+
+  char *ptr = BUFFER + BUFFER_CURRENT;
+
+  ssize_t const tmp = readlink(/*const char *restrict pathname =*/proc_self_exe,
+                               /*char *restrict buf =*/ptr,
+                               /*size_t bufsiz =*/SIZE_BUFFER - BUFFER_CURRENT);
+
+  ptr[tmp] = 0;
+
+  char *c = ptr + tmp - 1;
+
+  while (*c != '/') {
+    --c;
+  }
+
+  *c = 0;
+
+  while (*c != '/') {
+    --c;
+  }
+
+  c[1] = 'e';
+  c[2] = 'x';
+  c[3] = 'e';
+  c[4] = 0;
+
+  c = c + 5;
+
+  unsigned int const len = c - ptr;
+  BUFFER_CURRENT += len;
+
+  return ptr;
+}
+
+static inline char *get_ld(char const *path_dir_prefix) {
   size_t const len_name_ld = strlen(name_ld);
 
   size_t len_path_dir_prefix = strlen(path_dir_prefix);
 
   char *ret =
-      (char *)malloc((len_path_dir_prefix + len_name_ld + 1) * sizeof(char));
+      (char *)myalloc((len_path_dir_prefix + len_name_ld + 1) * sizeof(char));
 
   memcpy(/*void *dest =*/ret, /*const void *src =*/path_dir_prefix,
          /*size_t n =*/len_path_dir_prefix);
@@ -30,10 +73,10 @@ char *get_ld(char const *path_dir_prefix) {
   return ret;
 }
 
-char *get_name_exe(char *path_dir_prefix, char *name_exe) {
+static inline char *get_name_exe(char *path_dir_prefix, char *name_exe) {
   size_t len_name_exe = strlen(name_exe);
   size_t len_path_dir_prefix = strlen(path_dir_prefix);
-  char *ret = malloc((len_name_exe + len_path_dir_prefix + 2) * sizeof(char));
+  char *ret = myalloc((len_name_exe + len_path_dir_prefix + 2) * sizeof(char));
 
   memcpy(/*void *dest =*/ret, /*const void *src =*/path_dir_prefix,
          /*size_t n =*/len_path_dir_prefix);
@@ -49,7 +92,7 @@ char *get_name_exe(char *path_dir_prefix, char *name_exe) {
   return ret;
 }
 
-char *get_name(char *in) {
+static inline char *get_name(char *in) {
   char *ret = strrchr(/*const char *s =*/in, /*int c =*/'/');
   if (ret == NULL) {
     ret = in;
@@ -59,47 +102,13 @@ char *get_name(char *in) {
   return ret;
 }
 
-char *get_prefix_dir() {
-
-  ssize_t const tmp = readlink(/*const char *restrict pathname =*/proc_self_exe,
-                               /*char *restrict buf =*/BUFFER,
-                               /*size_t bufsiz =*/SIZE_BUFFER);
-
-  BUFFER[tmp] = 0;
-
-  char *c = BUFFER + tmp - 1;
-
-  while (*c != '/') {
-    --c;
-  }
-  *c = 0;
-  while (*c != '/') {
-    --c;
-  }
-
-  c[1] = 'e';
-  c[2] = 'x';
-  c[3] = 'e';
-  c[4] = 0;
-
-  c = c + 4;
-
-  unsigned int const len = c - BUFFER;
-
-  char *ret = malloc(len);
-
-  memcpy(/*void dest[restrict.n] =*/ret, /*const void src[restrict.n] =*/BUFFER,
-         /*size_t n =*/len);
-
-  return ret;
-}
-
 int main(int const argc, char **argv) {
+  BUFFER_CURRENT = 0;
+
   char *path_dir_prefix = get_prefix_dir();
   char *name_exe = get_name(/*char const * in =*/argv[0]);
-  // char name_exe[] = "alacritty";
   char *path_file_ld = get_ld(/*char const *path_dir_prefix =*/path_dir_prefix);
-  char **final_args = (char **)malloc((argc + 4) * sizeof(char *));
+  char **final_args = (char **)myalloc((argc + 4) * sizeof(char *));
 
   final_args[0] = path_file_ld;
   final_args[1] = library_path;
@@ -112,10 +121,6 @@ int main(int const argc, char **argv) {
     final_args[3 + i] = argv[i];
   }
 
-  /* for (int i = 0; i < argc + 3; ++i) { */
-  /*   printf("%s\n", final_args[i]); */
-  /* } */
-
   int ret = execv(/*const char *pathname =*/final_args[0],
                   /*char *const argv[] =*/final_args);
 
@@ -124,11 +129,6 @@ int main(int const argc, char **argv) {
   for (int i = 0; i < argc + 3; ++i) {
     printf("%s\n", final_args[i]);
   }
-
-  free(final_args[3]);
-  free(final_args);
-  free(path_file_ld);
-  free(path_dir_prefix);
 
   return ret;
 }
