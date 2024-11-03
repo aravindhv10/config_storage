c0feb863c9a8aade93687e021b75958ba9144604
aravind <aravindhv10@gmail.com>
2024-11-03 09:19:10 +0530

    Added few more scripts

--- RUST/all_install.sh
+++ RUST/all_install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-'./generic-1.sh' 'atuin'
+'./generic-1.sh' 'zellij'
---
+++ zellij_flatpak_wrapper/do_copy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -pv -- "${HOME}/bin" "${HOME}/exe"
+
+cp -vf -- './zellij' "${HOME}/bin/"
+
+cp -vf -- './enter_emacs_flatpak' "${HOME}/bin/"
+
+cp -vf -- './tmux_auto' "${HOME}/bin/"
