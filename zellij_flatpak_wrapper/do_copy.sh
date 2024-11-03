3c28e53e0cf4f2d30da66be101c0733b38e1eb94
aravind <aravindhv10@gmail.com>
2024-11-03 11:07:52 +0530

    More nice updates

--- zellij_flatpak_wrapper/do_copy.sh
+++ zellij_flatpak_wrapper/do_copy.sh
@@ -6,3 +6,7 @@ cp -vf -- './zellij' "${HOME}/bin/"
 cp -vf -- './enter_emacs_flatpak' "${HOME}/bin/"
 
 cp -vf -- './tmux_auto' "${HOME}/bin/"
+
+cp -vf -- './emd' "${HOME}/bin/"
+
+cp -vf -- './emc' "${HOME}/bin/"
