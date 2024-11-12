68e8b82f582589cec088c5b29d46b6f978fc038e
aravind <aravindhv10@gmail.com>
2024-11-12 19:57:56 +0530

    More improvements to docker template

--- docker_template_pytorch/host.docker_run_interactive.sh
+++ docker_template_pytorch/host.docker_run_interactive.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
-mkdir -pv -- "${2}"
+tail -n +5 "${0}" | tr '\n' ' ' > "${0}.slave.sh"
+exec sh "${0}.slave.sh" "${1}" "${2}"
+exit
 
-export INPUT="$(realpath -- "${1}")"
-export OUTPUT="$(realpath -- "${2}")"
+mkdir -pv -- "${2}" ;
+export INPUT="$(realpath -- "${1}")" ;
+export OUTPUT="$(realpath -- "${2}")" ;
+cd "$('dirname' '--' "${0}")" ;
+. './host.image_names.sh' ;
 
-cd "$('dirname' '--' "${0}")"
+sudo docker run
+--tty
+--interactive
+--rm
+--gpus all
+--ipc host
+--ulimit memlock=-1
+--ulimit stack=67108864
 
-. './host.image_names.sh'
+--mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472'
+-v "${INPUT}:/data/input"
+-v "${OUTPUT}:/data/output"
+-v "CACHE:/root/.cache"
 
-sudo docker run                                                          \
-    --tty                                                                \
-    --interactive                                                        \
-    --rm                                                                 \
-    --gpus all                                                           \
-    --ipc host                                                           \
-    --ulimit memlock=-1                                                  \
-    --ulimit stack=67108864                                              \
-    --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
-    -p '0.0.0.0:8888:8888/tcp'                                           \
-    -v "CACHE:/root/.cache"                                              \
-    -v "${INPUT}:/data/input"                                            \
-    -v "${OUTPUT}:/data/output"                                          \
-    "${IMAGE_NAME}"                                                      \
-    '/bin/bash'                                                          \
-;
+-p '0.0.0.0:8888:8888/tcp'
+
+"${IMAGE_NAME}"
+'/bin/bash' ;
