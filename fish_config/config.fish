89b5b9915a3d113cac1a7a1426de09f0575272df
aravind <aravindhv10@gmail.com>
2024-11-30 15:59:06 +0530

    Usual updates to debootstrap

--- fish_config/config.fish
+++ fish_config/config.fish
@@ -38,12 +38,12 @@ abbr --add --position command -- cat bat
 abbr --add --position command -- du dust
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 # =============================================================================
