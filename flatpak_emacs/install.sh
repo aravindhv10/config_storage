b857c79fd36e403c02358b2e221d9a7a82a2d868
Your Name <you@example.com>
2024-12-25 14:32:26 +0530

    Usual updates

--- flatpak_emacs/install.sh
+++ flatpak_emacs/install.sh
@@ -1,11 +1,56 @@
 #!/bin/sh
-flatpak search sdk > ./list.sdk.txt
-flatpak search llvm >> ./list.llvm.txt 
-flatpak search rust >> ./list.rust.txt
-flatpak search org.gnu.emacs >> ./list.emacs.txt
-
-flatpak install org.freedesktop.Sdk.Extension.llvm19/x86_64/24.08
-flatpak install org.freedesktop.Sdk.Extension.rust-stable/x86_64/24.08
-flatpak install org.freedesktop.Sdk/x86_64/24.08
+flatpak install com.brave.Browser
+flatpak install com.google.Chrome
+flatpak install com.slack.Slack
+flatpak install com.usebottles.bottles
+flatpak install com.valvesoftware.Steam
+flatpak install com.valvesoftware.Steam.CompatibilityTool.Proton-GE
+flatpak install com.visualstudio.code
+flatpak install dev.vieb.Vieb
+flatpak install dev.zed.Zed
+flatpak install fr.handbrake.ghb
+flatpak install fr.handbrake.ghb.Plugin.IntelMediaSDK
+flatpak install io.github.celluloid_player.Celluloid
+flatpak install io.gitlab.librewolf-community
+flatpak install io.mpv.Mpv
+flatpak install io.neovim.nvim
+flatpak install org.chromium.Chromium
+flatpak install org.chromium.Chromium.Codecs
+flatpak install org.contourterminal.Contour
+flatpak install org.deluge_torrent.deluge
+flatpak install org.freedesktop.Platform
+flatpak install org.freedesktop.Platform.Compat.i386
+flatpak install org.freedesktop.Platform.GL.default
+flatpak install org.freedesktop.Platform.GL32.default
+flatpak install org.freedesktop.Platform.ffmpeg-full
+flatpak install org.freedesktop.Platform.ffmpeg_full.i386
+flatpak install org.freedesktop.Platform.openh264
+flatpak install org.freedesktop.Sdk
+flatpak install org.freedesktop.Sdk.Extension.bazel
+flatpak install org.freedesktop.Sdk.Extension.llvm19
+flatpak install org.freedesktop.Sdk.Extension.rust-stable
+flatpak install org.freedesktop.Sdk.Extension.texlive
+flatpak install org.gimp.GIMP
+flatpak install org.gnome.Platform
+flatpak install org.gnome.Platform.Compat.i386
+flatpak install org.gnome.Sdk
 flatpak install org.gnu.emacs
-flatpak install org.kde.Sdk/x86_64/6.8
+flatpak install org.inkscape.Inkscape
+flatpak install org.kde.KStyle.Adwaita
+flatpak install org.kde.Platform
+flatpak install org.kde.Sdk
+flatpak install org.kde.kbibtex
+flatpak install org.kde.kolourpaint
+flatpak install org.libreoffice.LibreOffice
+flatpak install org.lyx.LyX
+flatpak install org.mozilla.firefox
+flatpak install org.qbittorrent.qBittorrent
+flatpak install org.texstudio.TeXstudio
+flatpak install org.tug.texworks
+flatpak install org.videolan.VLC
+flatpak install org.wezfurlong.wezterm
+flatpak install org.winehq.Wine.DLLs.dxvk
+flatpak install org.winehq.Wine.gecko
+flatpak install org.winehq.Wine.mono
+flatpak install tv.kodi.Kodi
+flatpak install us.zoom.Zoom
