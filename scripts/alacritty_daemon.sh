da21e51ff8571b96075b2e8841ed870cf779e111
Your Name <you@example.com>
2024-12-26 11:55:52 +0530

    Minor improvements

--- scripts/alacritty_daemon.sh
+++ scripts/alacritty_daemon.sh
@@ -2,8 +2,8 @@
 RES="$(fd 'Alacritty-wayland' "/run/user/$(id -u)/" | wc -l)"
 if test "${RES}" -gt 0
 then
-    alacritty msg create-window -e tmux_auto
+    alacritty msg create-window -e tmux_auto.sh
 else
-    alacritty -e tmux_auto
+    alacritty -e tmux_auto.sh
 fi
 exit '0'
