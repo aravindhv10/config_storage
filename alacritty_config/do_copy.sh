21dd3d4106776159317aa7db01a1f7e9a59c4353
Your Name <you@example.com>
2024-12-26 17:08:03 +0530

    Added important configs

---
+++ alacritty_config/do_copy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -pv -- "${HOME}/.config/alacritty/"
+cp -vf -- './alacritty.toml' "${HOME}/.config/alacritty/alacritty.toml"
---
+++ zram_config/do_copy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo cp -vf -- './zram-generator.conf' '/usr/lib/systemd/zram-generator.conf'
