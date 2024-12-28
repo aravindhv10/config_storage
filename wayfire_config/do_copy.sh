22cf6924c0935c38fa6b04390580a07c71e8c590
Your Name <you@example.com>
2024-12-28 22:01:51 +0530

    More wayfire config related stuff

--- wayfire_config/do_copy.sh
+++ wayfire_config/do_copy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd "$('dirname' '--' "${0}")"
+mkdir -pv -- "${HOME}/.config/waybar/"
 cp './wayfire.ini' "${HOME}/.config/wayfire.ini"
+cp './config.jsonc' "${HOME}/.config/waybar/config.jsonc"
 exit '0'
