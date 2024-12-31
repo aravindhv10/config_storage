2a52fc4150dfa9772896c25b0c9e0c6a845e6dec
Your Name <you@example.com>
2024-12-31 08:38:39 +0530

    More performance optimizations

--- scripts/alacritty_daemon.sh
+++ scripts/alacritty_daemon.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-RES="$(fd 'Alacritty-wayland' "/run/user/$(id -u)/" | wc -l)"
-if test "${RES}" -gt 0
+# RES="$(fd -ts -g -d1 'Alacritty-wayland-*.sock' "/run/user/$(id -u)/" | wc -l)"
+# fd -ts -F -d1 'Alacritty-wayland-' "/run/user/$(id -u)/"
+if fd -ts -g -d1 'Alacritty-wayland-*.sock$' "/run/user/$(id -u)/" -q
 then
     exec alacritty msg create-window ${@}
 else
