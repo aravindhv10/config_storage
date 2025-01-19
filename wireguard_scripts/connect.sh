f3746e7877276665e4a26c562e7202342d3a878c
Your Name <you@example.com>
2025-01-19 20:51:31 +0530

    More cleanups

--- wireguard_scripts/connect.sh
+++ wireguard_scripts/connect.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wg set wg0 listen-port 443 private-key ./privatekey peer "${PEER_KEY}" allowed-ips 10.0.0.2/24 endpoint "${IP}:${PORT}"
+wg set wg0 listen-port 443 private-key ./privatekey peer "${PEER_PUBLIC_KEY}" allowed-ips 10.0.0.2/24 endpoint "${IP}:${PORT}"
 ip link set wg0 up
