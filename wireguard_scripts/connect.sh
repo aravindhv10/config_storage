f181cf4d532aa671a564104b4a8dace20e708325
Your Name <you@example.com>
2025-01-20 10:45:54 +0530

    More wireguard scripts

--- wireguard_scripts/connect.sh
+++ wireguard_scripts/connect.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 . './config.sh'
-wg set wg0 listen-port "${PORT}" private-key ./privatekey peer "${PEER_PUBLIC_KEY}" allowed-ips "${PEER_INTERNAL_IP}/32" endpoint "${PEER_EXTERNAL_IP}:${PORT}"
+
+wg set wg0 \
+    listen-port "${SELF_PORT}" \
+    private-key ./privatekey \
+    peer "${PEER_PUBLIC_KEY}" \
+    allowed-ips "${PEER_INTERNAL_IP}/32" \
+    endpoint "${PEER_EXTERNAL_IP}:${PEER_PORT}" \
+;
+
 ip link set wg0 up
