cd236b85407dcbbdfd237faf1316663ef07d3090
Your Name <you@example.com>
2025-01-20 09:26:20 +0530

    More fixes

--- wireguard_scripts/connect.sh
+++ wireguard_scripts/connect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . './config.sh'
-wg set wg0 listen-port 443 private-key ./privatekey peer "${PEER_PUBLIC_KEY}" allowed-ips "${PEER_INTERNAL_IP}/32" endpoint "${PEER_EXTERNAL_IP}:${PORT}"
+wg set wg0 listen-port "${PORT}" private-key ./privatekey peer "${PEER_PUBLIC_KEY}" allowed-ips "${PEER_INTERNAL_IP}/32" endpoint "${PEER_EXTERNAL_IP}:${PORT}"
 ip link set wg0 up
