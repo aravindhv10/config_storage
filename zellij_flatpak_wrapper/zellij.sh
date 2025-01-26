4fa6d3084e4344c85d0c4cf003801964c386844f
Your Name <you@example.com>
2025-01-26 22:27:38 +0530

    Fixed a lot of bugs

--- zellij_flatpak_wrapper/zellij.sh
+++ zellij_flatpak_wrapper/zellij.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export PATH="${HOME}/exe:${HOME}/bin:/usr/lib/sdk/texlive/bin/x86_64-linux:/usr/lib/sdk/texlive/bin:/usr/lib/sdk/llvm19/bin:/usr/lib/sdk/rust-stable/bin:/app/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:"
-export SHELL="${HOME}/exe/nu"
-exec "${HOME}/exe/zellij" 'attach' '--create'
+export PATH="/var/tmp/all/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+export SHELL='nu'
+exec 'zellij' 'attach' '--create'
