d76573c0e2a5eecca69a95a866bf90276220d91c
Your Name <you@example.com>
2025-01-31 08:41:01 +0530

    More fixes to KDE config

--- kde_shortcuts/copy.sh
+++ kde_shortcuts/copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
-mkdir -pv "${HOME}/.local/share" "${HOME}/.config"
-cp -vf -- './kglobalshortcutsrc' "${HOME}/.config/"
-cp -vf -- './applications' "${HOME}/.local/share"
+mkdir -pv -- "${HOME}/.local/share" "${HOME}/.config"
+cp -vf    -- './kglobalshortcutsrc' "${HOME}/.config/"
+cp -vapf  -- './applications'       "${HOME}/.local/share"
 sudo find ./scripts -type f -exec cp {} /usr/local/bin/ -vf ';'
 exit '0'
