b673a0967a40a4653fcf5708b6b7cd930c69ae6e
Your Name <you@example.com>
2025-01-31 12:57:38 +0530

    Minor fixes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight bold :height 220 :width normal)))))
+ '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight bold :height 160 :width normal)))))
 
 (defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
