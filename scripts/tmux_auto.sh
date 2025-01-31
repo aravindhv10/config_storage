97ef1eb27830319a114991d2b8d5a0256080df3a
Your Name <you@example.com>
2025-01-31 13:14:23 +0530

    Added minor changes

--- scripts/tmux_auto.sh
+++ scripts/tmux_auto.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export PATH="/var/tmp/all/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 export SHELL='/var/tmp/all/bin/fish'
+export TERM='xterm-256color'
 exec byobu-tmux
 if tmux has
 then
