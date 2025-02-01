66ecdb2249399081f14c6fd2adfa50f99491acf9
Your Name <you@example.com>
2025-02-01 15:46:31 +0530

    More scripts work

--- kde_shortcuts/copy.sh
+++ kde_shortcuts/copy.sh
@@ -3,5 +3,5 @@ cd "$(dirname -- "${0}")"
 mkdir -pv -- "${HOME}/.local/share" "${HOME}/.config"
 cp -vf    -- './kglobalshortcutsrc' "${HOME}/.config/"
 cp -vapf  -- './applications'       "${HOME}/.local/share"
-sudo find ./scripts -name 'M_*' -type f -exec cp {} /usr/local/bin/ -vf ';'
+find ./scripts/ -type f | grep '^\./scripts/M.*_.*$' | sed 's@^@("cp" "-vf" "--" "@g ; s@$@" "/usr/local/bin/");@g' | sudo sh
 exit '0'
