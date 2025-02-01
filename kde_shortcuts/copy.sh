072d324b50717dccb86764c23ccbe0be23d73ffc
Your Name <you@example.com>
2025-02-01 15:58:13 +0530

    Regular scripts update

--- kde_shortcuts/copy.sh
+++ kde_shortcuts/copy.sh
@@ -3,5 +3,5 @@ cd "$(dirname -- "${0}")"
 mkdir -pv -- "${HOME}/.local/share" "${HOME}/.config"
 cp -vf    -- './kglobalshortcutsrc' "${HOME}/.config/"
 cp -vapf  -- './applications'       "${HOME}/.local/share"
-find ./scripts/ -type f | grep '^\./scripts/M.*_.*$' | sed 's@^@("cp" "-vf" "--" "@g ; s@$@" "/usr/local/bin/");@g' | sudo sh
+find './scripts/' '-type' 'f' | grep '^\./scripts/M.*_.*$' | sed 's@^@("cp" "-vf" "--" "@g ; s@$@" "/usr/local/bin/");@g' | sudo sh
 exit '0'
