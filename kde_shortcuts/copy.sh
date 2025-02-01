63cb97a89e215addf75ee1f1b39e6e6702c78afd
Your Name <you@example.com>
2025-02-01 14:15:26 +0530

    More scripts stuff

--- kde_shortcuts/copy.sh
+++ kde_shortcuts/copy.sh
@@ -3,5 +3,5 @@ cd "$(dirname -- "${0}")"
 mkdir -pv -- "${HOME}/.local/share" "${HOME}/.config"
 cp -vf    -- './kglobalshortcutsrc' "${HOME}/.config/"
 cp -vapf  -- './applications'       "${HOME}/.local/share"
-sudo find ./scripts -type f -exec cp {} /usr/local/bin/ -vf ';'
+sudo find ./scripts -name 'M_*' -type f -exec cp {} /usr/local/bin/ -vf ';'
 exit '0'
