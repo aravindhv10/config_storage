f4ce10153332e45826df53954f34dbc6d8cbef02
Your Name <you@example.com>
2025-02-06 21:11:39 +0530

    More fixes

--- zellij_flatpak_wrapper/zellij.sh
+++ zellij_flatpak_wrapper/zellij.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export PATH="/var/tmp/all/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+export PATH="/usr/lib/sdk/texlive/bin/x86_64-linux:/usr/lib/sdk/texlive/bin:/usr/lib/sdk/rust-stable/bin:/usr/lib/sdk/llvm19/bin:/var/tmp/all/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/app/bin"
 export SHELL='nu'
 exec 'zellij' 'attach' '--create'
