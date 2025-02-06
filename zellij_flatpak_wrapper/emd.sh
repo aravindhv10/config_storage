6413371bba3da4aed5b8b924887b9b5b0624a72e
Your Name <you@example.com>
2025-02-06 21:27:15 +0530

    More updates to scripts

--- zellij_flatpak_wrapper/emd.sh
+++ zellij_flatpak_wrapper/emd.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec emacs-wrapper --bg-daemon=def
+export PATH="/usr/lib/sdk/texlive/bin/x86_64-linux:/usr/lib/sdk/texlive/bin:/usr/lib/sdk/rust-stable/bin:/usr/lib/sdk/llvm19/bin:/var/tmp/all/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/app/bin"
+export SHELL='bash'
+exec flatpak run '--talk-name=org.freedesktop.Flatpak' "--command=/app/bin/emacsclient" 'org.gnu.emacs' '-c' "${@}"
