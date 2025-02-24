372ab4ca88475aabef13b379ecc2d0154df3a56a
Your Name <you@example.com>
2025-02-24 08:43:57 +0530

    Removed non existant packages

--- debootstrap/functions.sh
+++ debootstrap/functions.sh
@@ -129,9 +129,9 @@ function do_apt_install_standard {
     do_apt_install task-desktop task-gnome-desktop task-kde-desktop task-laptop task-lxqt-desktop
     do_apt_install task-ssh-server task-web-server task-xfce-desktop waypipe podman buildah guix nix-bin
     do_apt_install wireguard byobu tmux lxc lxc-templates lxctl distrobuilder libvirt-daemon-driver-lxc
-    do_apt_install qt5-style-kvantum qt5-style-kvantum-themes materia-kde python3-ipython ipython3
+    do_apt_install qt5-style-kvantum qt5-style-kvantum-themes python3-ipython ipython3
     do_apt_install arc-kde breeze-gtk-theme kde-style-oxygen-qt6 plasma-integration
-    do_apt_install materia-kde liboxygenstyle6-6 liboxygenstyleconfig6-6 qgnomeplatform-qt6
+    do_apt_install liboxygenstyle6-6 liboxygenstyleconfig6-6 qgnomeplatform-qt6
     do_apt_build tmux flatpak
 }
 
