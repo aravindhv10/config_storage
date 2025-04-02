a0a7c0734d2bceddd74750b232cdfd70f8c8dbba
asd <aravindhv10@gmail.com>
2025-04-02 17:39:47 +0530

    More rock torch stuff

--- ROCM_Torch/install.sh
+++ ROCM_Torch/install.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 uv venv "${HOME}/torch"
 . "${HOME}/torch/bin/activate"
-uv pip install -U pip
+
+uv pip install -U \
+    'pip' \
+    'ipython' \
+;
+
 pip install \
     '--index-url' 'https://download.pytorch.org/whl/rocm6.2.4' \
     'torch' \
     'torchvision' \
     'torchaudio' \
 ;
+
+uv pip install -U \
+    'qwen_vl_utils' \
+    'transformers' \
+    'accelerate' \
+;
+
+. "${HOME}/important_functions.sh"
+
+get_repo 'https://github.com/huggingface/accelerate.git'
+
+pip install -e .
+
+get_repo 'https://github.com/huggingface/accelerate.git'
+
+pip install -e .
