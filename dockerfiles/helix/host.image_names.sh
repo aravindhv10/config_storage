035ed083ebe7953f58931f8aee8972ba44399300
asd <aravindhv10@gmail.com>
2025-11-16 19:32:47 +0530

    Wrote container for helix

---
+++ dockerfiles/helix/host.image_names.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# IMAGE_NAME='debtestrustzsh'
+IMAGE_NAME='debtestrustzshhelix'
+
+BUILD_CONTAINER () {
+    CMD='sudo -A docker'
+    which buildah && CMD='buildah'
+    ${CMD} build -t "${IMAGE_NAME}" -f "./Dockerfile"
+}
+
+RUN_CONTAINER () {
+    CMD='sudo -A docker'
+    which podman && CMD='podman'
+    ${CMD} run -it --rm -v "$(realpath .):/data" "${IMAGE_NAME}" zsh
+}
