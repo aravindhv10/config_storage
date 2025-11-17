c9eeb8428089e95f4e9d8e3ae9f57d66b7daa971
asd <aravindhv10@gmail.com>
2025-11-17 11:11:48 +0530

    More refinements

---
+++ dockerfiles/rust_builder/host.image_names.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+IMAGE_NAME='debtestrustbuild'
+
+BUILD_CONTAINER () {
+    CMD='sudo -A docker'
+    which buildah && CMD='buildah'
+    ${CMD} build -t "${IMAGE_NAME}" -f "./Dockerfile"
+}
+
+RUN_CONTAINER () {
+    CMD='sudo -A docker'
+    which podman && CMD='podman'
+    ${CMD} run -it --rm -v "$(realpath .):/data" "${IMAGE_NAME}" bash
+}
+
+IMAGE_CMD='bash'
