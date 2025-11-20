42df1d842a36f255a679cc34cdb6f60f917116c9
Your Name <you@example.com>
2025-11-20 15:12:14 +0530

    Significant additions for cuda image

---
+++ dockerfiles/CUDA_UBUNTU/0_base/host.image_names.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+IMAGE_NAME='debtestrust'
+
+BUILD_CONTAINER () {
+    CMD='sudo -A docker'
+    which buildah && CMD='buildah'
+    ${CMD} build -t "${IMAGE_NAME}" -f "./Dockerfile"
+}
+
+RUN_CONTAINER () {
+    CMD='sudo -A docker'
+    which podman && CMD='podman'
+    ${CMD} run -it --rm -v "$(realpath .):/data" "${IMAGE_NAME}" bash
+}
---
+++ dockerfiles/CUDA_UBUNTU/5_libtorch/host.image_names.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# IMAGE_NAME='debtestrustzshhelix'
+IMAGE_NAME='debtestrustzshhelixpytorch'
+
+BUILD_CONTAINER () {
+    CMD='sudo -A docker'
+    which buildah && CMD='buildah'
+    ${CMD} build -t "${IMAGE_NAME}" -f "./Dockerfile"
+}
+
+RUN_CONTAINER () {
+    CMD='sudo -A docker'
+    which podman && CMD='podman'
+    ${CMD} run -it --rm \
+        -v "$(realpath .):/data" \
+        "${IMAGE_NAME}" zsh ;
+}
