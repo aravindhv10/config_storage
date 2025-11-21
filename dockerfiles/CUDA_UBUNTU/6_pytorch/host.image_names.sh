802d53176f6f6931d08949d5bef7ebe1b6c6ac00
Your Name <you@example.com>
2025-11-21 12:56:54 +0530

    Added nvidia options to docker run

--- dockerfiles/CUDA_UBUNTU/6_pytorch/host.image_names.sh
+++ dockerfiles/CUDA_UBUNTU/6_pytorch/host.image_names.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # IMAGE_NAME='debtestrustzshhelix'
 IMAGE_NAME='debtestrustzshhelixpytorch2'
+LISTEN_PORT='8080'
 
 BUILD_CONTAINER () {
     CMD='sudo -A docker'
@@ -12,6 +13,15 @@ RUN_CONTAINER () {
     CMD='sudo -A docker'
     which podman && CMD='podman'
     ${CMD} run -it --rm \
+        --gpus 'all,"capabilities=compute,utility,video"' \
+        --ipc host \
+        --ulimit memlock=-1 \
+        --ulimit stack=67108864 \
+        --shm-size 107374182400 \
+        --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
+        -v "CACHE:/root/.cache" \
+        -v "CACHE:/root/.triton" \
+        -p "0.0.0.0:${LISTEN_PORT}:${LISTEN_PORT}/tcp" \
         -v "$(realpath .):/data" \
         "${IMAGE_NAME}" zsh ;
 }
