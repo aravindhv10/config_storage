e3fbe1886726d9194954300f03279d3dddc12b38
asd <aravindhv10@gmail.com>
2025-11-24 20:40:50 +0530

    Minor updates to caching on rust builder image

--- dockerfiles/CPU/1_rust_builder/host.image_names.sh
+++ dockerfiles/CPU/1_rust_builder/host.image_names.sh
@@ -11,13 +11,14 @@ BUILD_CONTAINER () {
 RUN_CONTAINER () {
     CMD='sudo -A docker'
     which podman && CMD='podman'
-    mkdir -pv -- in out
+    mkdir -pv -- in out cache
 
     ${CMD} run -it --rm \
         -v "$(realpath .):/data" \
         -v "$(realpath .)/in:/root/GITHUB" \
         -v "$(realpath .)/in:/root/GITLAB" \
         -v "$(realpath .)/out:/var/tmp/" \
+        -v "$(realpath .)/cache:/usr/local/cargo/registry" \
         "${IMAGE_NAME}" bash ;
 }
 
