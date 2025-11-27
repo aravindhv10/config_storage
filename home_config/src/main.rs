ca7b84819f2de5b643648bda0766f552372c6347
asd <aravindhv10@gmail.com>
2025-11-27 22:07:12 +0530

    More updates to config builder

--- home_config/src/main.rs
+++ home_config/src/main.rs
@@ -1,4 +1,4 @@
-use std::{arch::x86_64::_MM_FROUND_CUR_DIRECTION, str::FromStr};
+use std::str::FromStr;
 
 fn get_env() -> std::collections::HashMap<std::ffi::OsString, std::ffi::OsString> {
     let mut current_env =
@@ -33,6 +33,10 @@ fn get_path_shrc(HOME: std::string::String) -> std::string::String {
     HOME + "/.shrc"
 }
 
+fn get_path_github(HOME: std::string::String) -> std::string::String {
+    HOME + "/GITHUB"
+}
+
 fn get_content_shrc() -> std::string::String {
     r#"
 export SUDO_ASKPASS="${HOME}/SUDO_ASKPASS"
@@ -58,6 +62,7 @@ struct configurator {
     path_home: std::string::String,
     path_shrc: std::string::String,
     path_zshrc: std::string::String,
+    path_github: std::string::String,
 }
 
 impl configurator {
@@ -66,20 +71,82 @@ impl configurator {
         let path_home = get_path_home(&current_env);
         let path_shrc = get_path_shrc(path_home.clone());
         let path_zshrc = get_path_zshrc(path_home.clone());
+        let path_github = get_path_github(path_home.clone());
         configurator {
             current_env: current_env,
             path_home: path_home,
             path_shrc: path_shrc,
             path_zshrc: path_zshrc,
+            path_github: path_github,
         }
+    }
+
+    fn get_github_repo(self: &Self, github_url: std::string::String) -> std::string::String {
+        // https://github.com/ohmyzsh/ohmyzsh.git
+
+        let starting_point = "https://github.com".len();
+        let ending_point = github_url.len() - ".git".len();
+        let new_url = self.path_github.clone() + &github_url.clone()[starting_point..ending_point];
+        let path = std::path::Path::new(new_url.as_str());
+
+        match std::fs::create_dir_all(path) {
+            Ok(_) => println!("Directories created successfully or already existed."),
+            Err(e) => eprintln!("Error creating directories: {}", e),
+        }
+
+        let command = std::process::Command::new("git")
+            .arg("clone")
+            .arg(github_url)
+            .arg(&new_url)
+            .output();
+
+        match command {
+            Ok(output) => {
+                // Check if the command executed successfully.
+                if output.status.success() {
+                    // Convert the stdout bytes to a UTF-8 string and print it.
+                    println!(
+                        "Command output: {}",
+                        String::from_utf8_lossy(&output.stdout)
+                    );
+                } else {
+                    // Print an error message if the command failed.
+                    eprintln!("Command failed with exit code: {}", output.status);
+                    eprintln!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+                }
+            }
+            Err(e) => {
+                // Handle any errors that occurred during command execution.
+                eprintln!("Failed to execute command: {}", e);
+            }
+        }
+
+        new_url
+    }
+
+    fn get_oh_my_zsh(self: &Self) {
+        let _ = self.get_github_repo("https://github.com/ohmyzsh/ohmyzsh.git".to_string());
+        match std::os::unix::fs::symlink(
+            "./GITHUB/ohmyzsh/ohmyzsh",
+            self.path_home.clone() + "/.oh-my-zsh",
+        ) {
+            Ok(_) => {
+                println!("Created oh-my-zsh symlink")
+            }
+            Err(e) => {
+                println!("failed to create symlink {}", e);
+            }
+        }
+    }
+
+    fn setup_shell(self: &Self) {
+        std::fs::write(&self.path_shrc, get_content_shrc()).expect("Unable to write shrc");
+        std::fs::write(&self.path_zshrc, get_content_zshrc()).expect("Unable to write zshrc");
+        self.get_oh_my_zsh();
     }
 }
 
 fn main() {
-    let current_env = get_env();
-    let path_home = get_path_home(&current_env);
-    let path_shrc = get_path_shrc(path_home.clone());
-    let path_zshrc = get_path_zshrc(path_home.clone());
-    std::fs::write(path_shrc, get_content_shrc()).expect("Unable to write shrc");
-    std::fs::write(path_zshrc, get_content_zshrc()).expect("Unable to write zshrc");
+    let slave = configurator::new();
+    slave.setup_shell();
 }
