686e92d9b704f7b36687ca6604d1ab4fb54b1fc6
Your Name <you@example.com>
2025-12-02 15:09:42 +0000

    Added more work on amdgpu

---
+++ dockerfiles/AMDGPU/4_python/host.image_names.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# IMAGE_NAME='debtestrustzshhelix'
+IMAGE_NAME='debtestrustzshhelixpy'
+
+BUILD_CONTAINER () {
+    CMD='sudo -A docker'
+    which buildah && CMD='buildah'
+    ${CMD} build -t "${IMAGE_NAME}" -f "./Dockerfile" .
+}
+
+RUN_CONTAINER () {
+    CMD='sudo -A docker'
+    which podman && CMD='podman'
+    ${CMD} run \
+        -it --rm \
+        '--device' '/dev/kfd' \
+        '--device' '/dev/dri' \
+        '--security-opt' 'seccomp=unconfined' \
+        -v "$(realpath .):/data" \
+        "${IMAGE_NAME}" zsh \
+    ;
+}
