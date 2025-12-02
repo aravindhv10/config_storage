561a7b966054a6df0113d8be1c446650068a7892
Your Name <you@example.com>
2025-12-02 14:37:09 +0000

    Changed scripts to run ROCM docker image

--- dockerfiles/AMDGPU/0_base/host.image_names.sh
+++ dockerfiles/AMDGPU/0_base/host.image_names.sh
@@ -10,5 +10,12 @@ BUILD_CONTAINER () {
 RUN_CONTAINER () {
     CMD='sudo -A docker'
     which podman && CMD='podman'
-    ${CMD} run -it --rm -v "$(realpath .):/data" "${IMAGE_NAME}" bash
+    ${CMD} run \
+        -it --rm \
+        '--device' '/dev/kfd' \
+        '--device' '/dev/dri' \
+        '--security-opt' 'seccomp=unconfined' \
+        -v "$(realpath .):/data" \
+        "${IMAGE_NAME}" bash \
+    l
 }
