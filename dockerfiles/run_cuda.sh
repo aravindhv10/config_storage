786d63730cafcad19b4130a488322254eebda724
Your Name <you@example.com>
2025-12-05 08:35:05 +0000

    more improvements to dockerfile

---
+++ dockerfiles/run_cuda.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+cd "$('dirname' '--' "${0}")"
+IMAGE_NAME="$(basename -- "$(realpath -- .)")"
+MKDIR './input'
+MKDIR './output'
+MKDIR './model'
+INPUT="$(realpath ./input)"
+OUTPUT="$(realpath ./output)"
+MODEL="$(realpath ./model)"
+sudo -A docker run \
+    --tty \
+    --interactive \
+    --rm \
+    --gpus 'all,"capabilities=compute,utility,video"' \
+    --ipc host \
+    --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
+    --shm-size 107374182400 \
+    --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
+    -v "${INPUT}:/data/input" \
+    -v "${OUTPUT}:/data/output" \
+    -v "${MODEL}:/data/model" \
+    -v "CACHE:/root/.cache" \
+    -v "CACHE:/root/.triton" \
+    -p "0.0.0.0:${LISTEN_PORT}:${LISTEN_PORT}/tcp" \
+    "${IMAGE_NAME}" "${IMAGE_CMD}" \
+;
