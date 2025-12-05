6d6eff4b2e97763aaad5630962c84c93c1763b3c
asd <aravindhv10@gmail.com>
2025-12-05 13:28:47 +0530

    Cleanups of dockerfiles

--- dockerfiles/CPU/1_rust_builder/install_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$(dirname -- "${0}")"
-ls /var/tmp/* -d | sed 's@^@("cp" "-vf" "./install.sh" "@g;s@$@");@g' | sh
--- dockerfiles/CPU/tmp_additions/image_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "$(dirname -- "${0}")"
-. './host.image_names.sh'
-RUN_CONTAINER
