d1c26869cf4b55d5127e5cfd6a502b4c06c54404
asd <aravindhv10@gmail.com>
2025-12-06 19:22:32 +0530

    Major rework of docker images

--- dockerfiles/build_cpu.sh
+++ dockerfiles/build_cpu.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 './build_container.sh' 'CPU/0_base'
-'./build_container.sh' 'CPU/1_rust_builder'
-'./build_container.sh' 'CPU/2_zsh'
-'./build_container.sh' 'CPU/3_helix'
+'./build_container.sh' 'CPU/1_install_packages'
+'./build_container.sh' 'CPU/2_install_rust'
+'./build_container.sh' 'CPU/3_good_setup'
 './build_container.sh' 'CPU/4_python'
 './build_container.sh' 'CPU/5_libtorch'
 './build_container.sh' 'CPU/6_pytorch'
--- dockerfiles/build_rocm_pytorch.sh
+++ dockerfiles/build_rocm_pytorch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 './build_container.sh' 'AMDGPU_PYT/0_base'
-'./build_container.sh' 'CPU/1_rust_builder'
-'./build_container.sh' 'CPU/2_zsh'
-'./build_container.sh' 'CPU/3_helix'
+'./build_container.sh' 'CPU/1_install_packages'
+'./build_container.sh' 'CPU/2_install_rust'
+'./build_container.sh' 'CPU/3_good_setup'
 './build_container.sh' 'CPU/4_python'
 './build_container.sh' 'AMDGPU_PYT/5_libtorch'
 './build_container.sh' 'AMDGPU_PYT/6_pytorch'
