5a7000db47273306994b47c2a569abd431bd630b
Your Name <you@example.com>
2025-12-15 10:12:47 +0000

    More work on nvcr images

--- dockerfiles/build_cuda_official.sh
+++ dockerfiles/build_cuda_official.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
+'./build_container.sh' 'CUDA_UBUNTU_OFFICIAL/0_base'
+'./build_container.sh' 'CUDA_UBUNTU_OFFICIAL/1_install_packages'
+'./build_container.sh' 'CPU/2_install_rust'
+'./build_container.sh' 'CPU/3_good_setup'
+'./build_container.sh' 'CPU/4_python'
+'./build_container.sh' 'CUDA_UBUNTU_OFFICIAL/5_libtorch'
 './build_container.sh' 'CUDA_UBUNTU_OFFICIAL/6_pytorch'
 exit '0'
