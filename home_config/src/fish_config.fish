0ebc0d400168ea23f873601070e09fc0ec75dc00
asd <aravindhv10@gmail.com>
2025-12-30 21:07:24 +0530

    Updated home config codes

--- home_config/src/fish_config.fish
+++ home_config/src/fish_config.fish
@@ -2,3 +2,12 @@ export SHELL=fish
 . "$HOME/.shrc"
 
 fish_vi_key_bindings
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
