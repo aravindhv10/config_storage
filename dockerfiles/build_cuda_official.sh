8bca33f90ede46566f4a1d7d78eaf7856926f54b
asd <aravindhv10@gmail.com>
2026-01-06 19:01:07 +0530

    More cleanups and refinements

--- dockerfiles/build_cuda_official.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "$(dirname -- "${0}")"
-echo 'Deprecated, run the ./build_cuda.sh'
-exit '0'
--- dockerfiles/build_rocm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "$(dirname -- "${0}")"
-echo 'Deprecated, run the ./build_rocm_pytorch.sh'
-exit '0'
