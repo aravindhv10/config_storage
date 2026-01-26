121a0667541d9b3ebe64ce9f26e368d6017b079c
Your Name <you@example.com>
2026-01-26 19:23:42 +0530

    updated home config

--- home_config/src/fish_config.fish
+++ home_config/src/fish_config.fish
@@ -4,11 +4,10 @@ export SUDO_ASKPASS="$HOME/SUDO_ASKPASS"
 fish_vi_key_bindings
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
-
