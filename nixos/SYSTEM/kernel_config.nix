92ed3c1def2357865f5ba183b93a3e004a98b6ed
asd <aravindhv10@gmail.com>
2026-02-21 20:48:26 +0530

    Updating nixos configs

--- nixos/SYSTEM/kernel_config.nix
+++ nixos/SYSTEM/kernel_config.nix
@@ -14,7 +14,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiInstallAsRemovable = false;
   boot.loader.grub.efiSupport = true;
   boot.loader.systemd-boot.enable = false;
   boot.tmp.tmpfsSize = "60%";
