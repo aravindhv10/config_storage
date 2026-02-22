cf83456d73510b7799fc12827da6937884b82ae3
asd <aravindhv10@gmail.com>
2026-02-22 08:31:48 +0530

    Updating nixos configs

--- nixos/SYSTEM/services.nix
+++ nixos/SYSTEM/services.nix
@@ -5,6 +5,8 @@
   modulesPath,
   ...
 }: {
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
   services.xserver = {
     enable = true;
     videoDrivers = ["amdgpu"];
