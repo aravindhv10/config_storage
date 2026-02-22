0fa4a43554580dd8317e948ea7bb3c6b9fcd0dd6
asd <aravindhv10@gmail.com>
2026-02-22 11:16:47 +0530

    Updating nixos configs

--- nixos/SYSTEM/services.nix
+++ nixos/SYSTEM/services.nix
@@ -8,11 +8,27 @@
   services = {
     chrony.enable = true;
     openssh.enable = true;
+    printing.enable = true;
 
     desktopManager = {
       plasma6.enable = true;
       gnome.enable = true;
     };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    libinput.enable = true;
 
     # Enable the X11 windowing system.
     # You can disable this if you're only using the Wayland session.
