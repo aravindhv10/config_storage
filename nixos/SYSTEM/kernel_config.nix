f89c83534fb3006b61b2c6b3ff73fe909d588115
asd <aravindhv10@gmail.com>
2026-02-22 10:14:17 +0530

    Updating nixos configs

--- nixos/SYSTEM/kernel_config.nix
+++ nixos/SYSTEM/kernel_config.nix
@@ -29,5 +29,23 @@
     kernelModules = ["kvm-amd" "amdgpu"];
     kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
     kernelParams = ["zswap.enabled=1" "zswap.max_pool_percent=80"];
+
+    # kernelPackages =
+    # let
+    #     linux_sgx_pkg = { fetchurl, buildLinux, ... } @ args:
+    #         buildLinux (
+    #             args // rec {
+    #                 version = "6.13.11-xanmod1" ;
+    #                 modDirVersion = version;
+    #                 src = /home/asd/GITLAB/xanmod/linux-6.13.11.tar; # /home/asd/GITLAB/xanmod/linux-6.12.19.tar;
+    #                 kernelPatches = [];
+    #                 extraConfig = ''
+    #                 '';
+    #                 extraMeta.branch = version ;
+    #             } // (args.argsOverride or {})
+    #         );
+    #     linux_sgx = pkgs.callPackage linux_sgx_pkg{};
+    # in
+    #     pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_sgx);
   };
 }
