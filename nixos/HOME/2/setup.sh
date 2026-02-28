cd2ed925f9463972f3a2776c60ce888528402f77
asd <aravindhv10@gmail.com>
2026-02-28 21:46:15 +0530

    updating wayfire and waybar config

---
+++ nixos/HOME/2/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "$('dirname' -- "${0}")"
+
+C(){
+    mkdir -pv -- "$('dirname' -- "${2}")"
+    rm -vf -- "${2}"
+    cp -vf -- "${1}" "${2}"
+}
+
+C 'HOME,.config,wayfire.ini' "${HOME}/.config/wayfire.ini"
+
+C './HOME,.config,waybar,config.jsonc' "${HOME}/.config/waybar/config.jsonc"
