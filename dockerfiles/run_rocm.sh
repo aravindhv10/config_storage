8dc171c23c1142f023ff235c228e15e41828325a
asd <aravindhv10@gmail.com>
2026-02-28 20:31:17 +0530

    Updated docker stuff

--- dockerfiles/run_rocm.sh
+++ dockerfiles/run_rocm.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 cd "$('dirname' '--' "${0}")"
+
+# IMAGE_NAME="6_pytorch"
 IMAGE_NAME="$(basename -- "$(realpath -- .)")"
+
+IMAGE_CMD='zsh'
+
+PATH_DIR_SRC="$('realpath' '.')"
+PATH_DIR_DST="/data/$('basename' -- "${PATH_DIR_SRC}")"
+
 podman run \
     -it --rm \
     '--device' '/dev/kfd' \
@@ -10,7 +18,8 @@ podman run \
     --mount 'type=tmpfs,destination=/data/TMPFS,tmpfs-size=137438953472' \
     -v "$(realpath .):/data/source" \
     -v "${HOME}/BUILD:/data/build" \
+    # -v "${PATH_DIR_SRC}:${PATH_DIR_DST}" \
     -v "CACHE:/usr/local/cargo/registry" \
     -v "CACHE:/root/.cache" \
-    "${IMAGE_NAME}" zsh \
+    "${IMAGE_NAME}" "${IMAGE_CMD}" \
 ;
