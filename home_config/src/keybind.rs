3047403304d2b0eb058f6d9e656616cec025ee19
asd <aravindhv10@gmail.com>
2026-03-01 21:05:10 +0530

    Started multicall binary

--- home_config/src/keybind.rs
+++ home_config/src/keybind.rs
@@ -1,3 +1,6 @@
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+
 fn main() {
     // Get the first argument, which is the path used to invoke the binary
     let path = std::env::args().next().expect("No executable path found");
@@ -10,10 +13,33 @@ fn main() {
 
     // Dispatch based on the name
     match binary_name {
-        "M_C_A" => ,
+        "M_C_Q" => {
+            let err = Command::new("wezterm").exec();
+        }
+        "M_C_W" => {}
+        "M_C_E" => {}
+        "M_C_R" => {}
+        "M_C_T" => {}
+        "M_C_A" => {
+            let err = Command::new("firefox").exec();
+        }
+        "M_C_S" => {
+            let err = Command::new("thorium")
+                .arg("--enable-features=UseOzonePlatform")
+                .arg("--ozone-platform=wayland")
+                .exec();
+        }
+        "M_C_D" => {
+            let err = Command::new("dolphin").exec();
+        }
+        "M_C_F" => {
+            let err = Command::new("pavucontrol").exec();
+        }
+        "M_C_G" => {
+            let err = Command::new("footclient").arg("nmtui").exec();
+        }
         _ => {
             println!("Unknown command: {}. Defaulting to help.", binary_name);
-            show_help();
         }
     }
 }
