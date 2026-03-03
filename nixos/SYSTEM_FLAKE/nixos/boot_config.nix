0ac668a625c2488c214786b6974e8169d92cfc65
asd <aravindhv10@gmail.com>
2026-03-03 22:01:03 +0530

    Added thorium flake

--- nixos/SYSTEM_FLAKE/nixos/boot_config.nix
+++ nixos/SYSTEM_FLAKE/nixos/boot_config.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  unstable,
-  ...
-}: {
+{pkgs, ...}: {
   boot = {
     loader = {
       efi = {
@@ -36,28 +29,5 @@
     kernelModules = ["kvm-amd" "amdgpu"];
     kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
     kernelParams = ["zswap.enabled=1" "zswap.max_pool_percent=80"];
-
-    # kernelPackages = pkgs.linuxPackages_6_14;
-    # kernelPackages = pkgs.linuxPackages_6_15;
-    # kernelPackages = pkgs.linuxPackages_6_16;
-    # kernelPackages = pkgs.linuxPackages_6_17;
-
-    # kernelPackages =
-    # let
-    #     linux_sgx_pkg = { fetchurl, buildLinux, ... } @ args:
-    #         buildLinux (
-    #             args // rec {
-    #                 version = "6.13.11-xanmod1" ;
-    #                 modDirVersion = version;
-    #                 src = /home/asd/GITLAB/xanmod/linux-6.13.11.tar; # /home/asd/GITLAB/xanmod/linux-6.12.19.tar;
-    #                 kernelPatches = [];
-    #                 extraConfig = ''
-    #                 '';
-    #                 extraMeta.branch = version ;
-    #             } // (args.argsOverride or {})
-    #         );
-    #     linux_sgx = pkgs.callPackage linux_sgx_pkg{};
-    # in
-    #     pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_sgx);
   };
 }
