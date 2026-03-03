4cbac71ad61551c9702ec308c0803e09f34477ec
asd <aravindhv10@gmail.com>
2026-03-03 22:04:09 +0530

    Added thorium flake

--- nixos/SYSTEM_FLAKE/nixos/network_config.nix
+++ nixos/SYSTEM_FLAKE/nixos/network_config.nix
@@ -1,13 +1,5 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  unstable,
-  ...
-}: {
+{lib, ...}: {
   networking = {
-    # Define your hostname.
     hostName = "nixos";
 
     hosts = {
@@ -19,12 +11,5 @@
     nftables.enable = true;
     timeServers = ["ntp.example.com" "time.google.com"];
     useDHCP = lib.mkDefault true;
-
-    # interfaces.wlp1s0.useDHCP = lib.mkDefault true;
-    # wireless.enable = true;
-    # proxy.default = "http://user:password@proxy:port/";
-    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
   };
 }
