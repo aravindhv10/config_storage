1fd3c2e16ce1fc712b2a3347dd6cfa9b86a3e622
asd <aravindhv10@gmail.com>
2026-03-15 12:45:43 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,18 +1,14 @@
-use ndarray::Array3;
-use std::io::Cursor;
-use video_rs::{self, Decoder, Options, Receiver};
-
 async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
     let buffer = tokio::fs::read(path_file_video_input).await?;
-
-    video_rs::init().unwrap();
-    let source = Cursor::new(buffer);
+    let reader = std::io::Cursor::new(buffer);
+    let mut ictx = ffmpeg_next::format::io::input(reader).unwrap();
 
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    ffmpeg_next::init().unwrap();
     let _ = read_video("./video.mp4".to_string()).await;
     Ok(())
 }
