e0541619401eb6f40d6cee13cb541c26f7838a55
asd <aravindhv10@gmail.com>
2026-03-15 22:52:05 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,10 +1,21 @@
 async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
+    let path_dir_images_output = path_file_video_input.clone() + ".dir";
+    tokio::fs::create_dir_all(path_dir_images_output.as_str()).await?;
+    let res = tokio::process::Command::new("ffmpeg")
+        .args([
+            "-i",
+            path_file_video_input.as_str(),
+            "-r",
+            "8",
+            (path_dir_images_output + "/out-%6d.bmp").as_str(),
+        ])
+        .spawn();
+
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    ffmpeg::init().unwrap();
     let _ = read_video("./video.mp4".to_string()).await;
     Ok(())
 }
