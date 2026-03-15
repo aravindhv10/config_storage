b9fc63029370216ec07f5ca64fb020f8be902fc5
asd <aravindhv10@gmail.com>
2026-03-15 12:15:01 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,4 +1,6 @@
-fn read_video(path_file_video_input: String) {}
+async fn read_video(path_file_video_input: String) {
+    let data = tokio::fs::read(path_file_video_input).await?;
+}
 
 fn main() {
     println!("Hello, world!");
