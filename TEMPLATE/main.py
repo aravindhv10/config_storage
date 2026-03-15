4be49caf6b8b42ffe2758588bd2ab8a70bcc397e
asd <aravindhv10@gmail.com>
2026-03-15 11:32:21 +0530

    Added template dir to the repo

---
+++ TEMPLATE/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+
+try:
+    __file__
+except:
+    basepath = "."
+else:
+    basepath = os.path.abspath(os.path.dirname(__file__) + "/")
+import sys
+
+sys.path.append(basepath)
+from important_common_utils import *
+import numpy as np
+import torch
