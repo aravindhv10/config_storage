a0de740bf0128f603dabca1fbb57c822df830b69
asd <aravindhv10@gmail.com>
2026-03-15 23:06:36 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,3 +1,5 @@
+use std::num::IntErrorKind;
+
 async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
     let path_dir_images_output = path_file_video_input.clone() + ".dir";
     tokio::fs::create_dir_all(path_dir_images_output.as_str()).await?;
@@ -7,9 +9,21 @@ async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
             path_file_video_input.as_str(),
             "-r",
             "8",
-            (path_dir_images_output + "/out-%6d.bmp").as_str(),
+            (path_dir_images_output.clone() + "/out-%6d.bmp").as_str(),
         ])
         .spawn();
+
+    for entry in
+        glob(path_dir_images_output.clone() + "/out-*.bmp").expect("Failed to read glob pattern")
+    {
+        match entry {
+            Ok(path) => {
+                println!("{:?}", path.display())
+                tokio::fs::remove_file(path.display());
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
 
     Ok(())
 }
