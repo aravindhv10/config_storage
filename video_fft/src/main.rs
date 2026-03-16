b9f23f3c0d298058f7d9c42d9a12c3d8932bd0e8
asd <aravindhv10@gmail.com>
2026-03-16 11:38:54 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,6 +1,10 @@
 use std::num::IntErrorKind;
 
-async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
+async fn video_2_images(path_file_video_input: String) -> Vec<String> {
+    Vec![]
+}
+
+async fn read_video_to_images(path_file_video_input: String) -> anyhow::Result<()> {
     let path_dir_images_output = path_file_video_input.clone() + ".dir";
     tokio::fs::create_dir_all(path_dir_images_output.as_str()).await?;
     let res = tokio::process::Command::new("ffmpeg")
@@ -35,8 +39,35 @@ async fn read_video(path_file_video_input: String) -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn read_video_to_raw(
+    path_file_video_input: String,
+    fps: f32,
+    size_x: u32,
+    size_y: u32,
+) -> anyhow::Result<()> {
+    let path_file_video_output = path_file_video_input.clone() + ".raw";
+    let res = tokio::process::Command::new("ffmpeg")
+        .args([
+            "-i",
+            path_file_video_input.as_str(),
+            "-r",
+            "{fps}",
+            "-f",
+            "rawvideo",
+            "-pix_fmt",
+            "rgb24",
+            "-vf",
+            "scale=1280:720",
+            path_file_video_output.as_str(),
+        ])
+        .status()
+        .await;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let _ = read_video("./video.mp4".to_string()).await;
+    let _ = read_video_to_raw("./video.mp4".to_string()).await;
     Ok(())
 }
