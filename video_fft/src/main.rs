a7f18037b5893fde6f77e78fcabacc38bb6a10fc
asd <aravindhv10@gmail.com>
2026-03-16 19:13:30 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -1,4 +1,4 @@
-async fn convert_video_to_blob(
+async fn convert_encoded_video_to_raw(
     path_file_video_input: String,
     path_file_video_output: String,
     fps: f32,
@@ -53,40 +53,14 @@ async fn read_video_to_raw(
     size_y: u32,
 ) -> anyhow::Result<()> {
     let path_file_video_output = path_file_video_input.clone() + ".raw";
-    if !tokio::fs::try_exists(path_file_video_output.as_str()).await? {
-        let res = tokio::process::Command::new("ffmpeg")
-            .args([
-                "-i",
-                path_file_video_input.as_str(),
-                "-r",
-                fps.to_string().as_str(),
-                "-nostdin",
-                "-f",
-                "rawvideo",
-                "-pix_fmt",
-                "rgb24",
-                "-vf",
-                ("scale=".to_string()
-                    + size_x.to_string().as_str()
-                    + ":"
-                    + size_y.to_string().as_str())
-                .as_str(),
-                path_file_video_output.as_str(),
-            ])
-            .status()
-            .await?;
 
-        match res.code() {
-            None => {
-                return Err(anyhow::format_err!("FFmpeg failed with unknown error"));
-            }
-            Some(e) => {
-                return Err(anyhow::format_err!("FFmpeg failed with error code {}", e));
-            }
-        };
-    } else {
-        eprintln!("Not running ffmpeg, output file already exists.");
-    }
+    convert_encoded_video_to_raw(
+        path_file_video_input,
+        path_file_video_output,
+        fps,
+        size_x,
+        size_y,
+    ).await?
 
     let file =
         std::fs::File::open(path_file_video_output.as_str()).expect("failed to open the file");
