e31b2a28bd92e9b0b91ca254371aa65b11dba432
asd <aravindhv10@gmail.com>
2026-03-16 19:06:28 +0530

    Started work on video fft

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -5,27 +5,40 @@ async fn read_video_to_raw(
     size_y: u32,
 ) -> anyhow::Result<()> {
     let path_file_video_output = path_file_video_input.clone() + ".raw";
-    let res = tokio::process::Command::new("ffmpeg")
-        .args([
-            "-i",
-            path_file_video_input.as_str(),
-            "-r",
-            fps.to_string().as_str(),
-            "-nostdin",
-            "-f",
-            "rawvideo",
-            "-pix_fmt",
-            "rgb24",
-            "-vf",
-            ("scale=".to_string()
-                + size_x.to_string().as_str()
-                + ":"
-                + size_y.to_string().as_str())
-            .as_str(),
-            path_file_video_output.as_str(),
-        ])
-        .status()
-        .await?;
+    if !tokio::fs::try_exists(path_file_video_output.as_str()).await? {
+        let res = tokio::process::Command::new("ffmpeg")
+            .args([
+                "-i",
+                path_file_video_input.as_str(),
+                "-r",
+                fps.to_string().as_str(),
+                "-nostdin",
+                "-f",
+                "rawvideo",
+                "-pix_fmt",
+                "rgb24",
+                "-vf",
+                ("scale=".to_string()
+                    + size_x.to_string().as_str()
+                    + ":"
+                    + size_y.to_string().as_str())
+                .as_str(),
+                path_file_video_output.as_str(),
+            ])
+            .status()
+            .await?;
+
+        match res.code() {
+            None => {
+                return Err(anyhow::format_err!("FFmpeg failed with unknown error"));
+            }
+            Some(e) => {
+                return Err(anyhow::format_err!("FFmpeg failed with error code {}", e));
+            }
+        };
+    } else {
+        eprintln!("Not running ffmpeg, output file already exists.");
+    }
 
     let file =
         std::fs::File::open(path_file_video_output.as_str()).expect("failed to open the file");
