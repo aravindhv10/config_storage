0d977344359f9d0dbf09ba576f85e92fe27cddb9
asd <aravindhv10@gmail.com>
2026-03-19 19:51:57 +0530

    Some comments in the code

--- video_fft/src/export.hpp
+++ video_fft/src/export.hpp
@@ -14,6 +14,9 @@ using uint32_t = unsigned int;
 using uint64_t = unsigned long;
 using uint8_t = unsigned char;
 
+using intype = uint8_t;
+using outtype = float32_t;
+
 int do_fft_compress(void *blob, uint16_t size_t, uint16_t size_y,
                     uint16_t size_x, uint8_t size_c, float32_t fps,
                     float32_t freq_limit, void *dest);
