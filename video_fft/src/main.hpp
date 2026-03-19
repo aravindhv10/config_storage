e41b9f80caa028dab5f3740c5f564f13031fbd61
asd <aravindhv10@gmail.com>
2026-03-19 11:22:17 +0530

    Some comments in the code

--- video_fft/src/main.hpp
+++ video_fft/src/main.hpp
@@ -1,3 +1,6 @@
+#ifndef _HEADER_GUARD_src_main_hpp
+#define _HEADER_GUARD_src_main_hpp
+
 #include <iostream>
 #include <vector>
 
@@ -7,3 +10,5 @@
 
 #include <torch/csrc/inductor/aoti_package/model_package_loader.h>
 #include <torch/torch.h>
+
+#endif
