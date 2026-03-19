c5e6e82a126bbc93172482ed224a28ad279bcd51
asd <aravindhv10@gmail.com>
2026-03-19 08:52:18 +0530

    Some comments in the code

--- video_fft/src/main.cpp
+++ video_fft/src/main.cpp
@@ -1,7 +1,1 @@
-#include <iostream>
-#include <vector>
-
-#include <torch/csrc/inductor/aoti_package/model_package_loader.h>
-#include <torch/torch.h>
-
-#include "./main.cpp"
+#include "./main.hpp"
