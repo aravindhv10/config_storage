154de129cf6c33e52d076807de82197b4069f93f
asd <aravindhv10@gmail.com>
2026-03-19 12:16:33 +0530

    Some comments in the code

--- video_fft/cmake_build.sh
+++ video_fft/cmake_build.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
+
 export LIBTORCH_USE_PYTORCH=1
 export RUSTFLAGS="-C target-cpu=native"
+
 mkdir -pv -- 'target'
 cd 'target'
-cmake ..
-make -j
+
+rm -rf -- 'CMakeFiles'
+
+unlink 'CMakeCache.txt'
+unlink 'cmake_install.cmake'
+unlink 'compile_commands.json'
+unlink 'libmytorch.so'
+unlink 'libmytorch.so.1.0'
+unlink 'Makefile'
+
+cmake .. ; make -j
