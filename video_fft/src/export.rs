ba0651be77f95887292214c1d4008b08e946bf8f
asd <aravindhv10@gmail.com>
2026-03-19 12:10:17 +0530

    Some comments in the code

---
+++ video_fft/src/export.rs
@@ -0,0 +1,15 @@
+pub type float32_t = f32;
+pub type float64_t = f64;
+
+unsafe extern "C" {
+    pub fn do_fft_compress(
+        blob: *mut ::std::os::raw::c_void,
+        size_t: u16,
+        size_y: u16,
+        size_x: u16,
+        size_c: u8,
+        fps: float32_t,
+        freq_limit: float32_t,
+        dest: *mut ::std::os::raw::c_void,
+    ) -> ::std::os::raw::c_int;
+}
