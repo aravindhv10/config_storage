ffd3e234756a2cd19a5db808284d9012aa3433d4
asd <aravindhv10@gmail.com>
2026-03-19 12:05:59 +0530

    Some comments in the code

--- video_fft/src/export.hpp
+++ video_fft/src/export.hpp
@@ -1,5 +1,6 @@
 #ifndef _HEADER_GUARD_src_export_hpp
 #define _HEADER_GUARD_src_export_hpp
+extern "C" {
 
 using float32_t = float;
 using float64_t = double;
@@ -12,10 +13,9 @@ using uint32_t = unsigned int;
 using uint64_t = unsigned long;
 using uint8_t = unsigned char;
 
-extern "C" {
 int do_fft_compress(void *blob, uint16_t size_t, uint16_t size_y,
                     uint16_t size_x, uint8_t size_c, float32_t fps,
                     float32_t freq_limit, void *dest);
-}
 
+} // extern "C"
 #endif
