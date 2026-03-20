9fee0e55513127718ebc0f4b04f731bd4caf4723
asd <aravindhv10@gmail.com>
2026-03-20 12:10:31 +0530

    Some comments in the code

--- video_fft/src/main.cpp
+++ video_fft/src/main.cpp
@@ -56,8 +56,8 @@ inline torch::Tensor do_pad_video(torch::Tensor tensor_input) {
 }
 
 extern "C" {
-int do_fft_compress(void const * const blob, uint16_t const len_t, uint16_t const len_y, uint16_t const len_x,
-                    uint8_t const len_c, float32_t const fps, float32_t const freq_limit, void const * const dest) {
+int do_fft_compress(void * const blob, uint16_t const len_t, uint16_t const len_y, uint16_t const len_x,
+                    uint8_t const len_c, float32_t const fps, float32_t const freq_limit, void * const dest) {
 
   int64_t dist_c = 1;
   int64_t dist_x = len_c * dist_c;
