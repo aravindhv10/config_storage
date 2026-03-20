e39be32fe1a619cfa1f420621dfdc4e8420dc0d3
asd <aravindhv10@gmail.com>
2026-03-20 10:14:41 +0530

    Some comments in the code

--- video_fft/src/export.hpp
+++ video_fft/src/export.hpp
@@ -11,11 +11,7 @@ using float64_t = double;
 using intype = unsigned char;
 using outtype = float;
 
-int do_fft_compress(void *blob, uint16_t size_t, uint16_t size_y,
-                    uint16_t size_x, uint8_t size_c, float32_t fps,
-                    float32_t freq_limit, void *dest);
-
-int do_debug();
+#include "./export.cpp"
 
 } // extern "C"
 
