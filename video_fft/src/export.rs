035ecb395371c9b988fe3930d6980054be4e16ca
asd <aravindhv10@gmail.com>
2026-03-21 07:58:31 +0530

    Added more docker scripts

--- video_fft/src/export.rs
+++ video_fft/src/export.rs
@@ -7,12 +7,13 @@ pub type outtype = float32_t;
 unsafe extern "C" {
     pub fn do_fft_compress(
         blob: *mut ::std::os::raw::c_void,
-        size_t: u16,
-        size_y: u16,
-        size_x: u16,
-        size_c: u8,
+        len_t: u16,
+        len_y: u16,
+        len_x: u16,
+        len_c: u8,
         fps: float32_t,
         freq_limit: float32_t,
         dest: *mut ::std::os::raw::c_void,
+        use_gpu: bool,
     ) -> ::std::os::raw::c_int;
 }
