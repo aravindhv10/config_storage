89c6b48a0b3ac8c289ab3d8c5c816a3ab433319d
asd <aravindhv10@gmail.com>
2026-03-22 20:01:25 +0530

    Mostly done with very efficient video fft code

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -583,7 +583,7 @@ fn main() -> anyhow::Result<()> {
         if !path.is_dir() {
             if let Some(ext) = path.extension() {
                 if ext == ".mp4" {
-                    ist_path_file_video.push(path.display());
+                    list_path_file_video.push(path.display());
                 }
             }
         }
