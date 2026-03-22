68685513bafa0df8b4b9461a0865248a82fb5ffc
asd <aravindhv10@gmail.com>
2026-03-22 11:19:54 +0530

    Added more docker scripts

--- video_fft/convert_video_2_raw.sh
+++ video_fft/convert_video_2_raw.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 cd "$('dirname' -- "${0}")"
-exec ffmpeg "-i" './video.mp4' "-r" '8' "-nostdin" "-f" "rawvideo" "-pix_fmt" "rgb24" "-vf" "scale=1280:720" './video.mp4.raw'
+ffmpeg \
+    "-i" './video.mp4' \
+    "-r" '8' \
+    "-nostdin" \
+    "-f" "rawvideo" \
+    "-pix_fmt" "rgb24" \
+    "-vf" "scale=1280:720" \
+    './video.mp4.raw' \
+;
+exit '0'
