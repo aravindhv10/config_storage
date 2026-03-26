0cb23544524680c5f4165afeba9ed0f750ed61e6
asd <aravindhv10@gmail.com>
2026-03-26 12:05:33 +0530

    changed to use /dev/shm for temporary video location

--- video_fft/src/main.rs
+++ video_fft/src/main.rs
@@ -3,6 +3,11 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
+mod videofn;
+use videofn::convert_encoded_video_to_raw;
+use videofn::get_file_hash;
+use videofn::get_str_hash;
+
 use anyhow::Context;
 use bytemuck::Contiguous;
 use bytemuck::Pod;
@@ -16,72 +21,6 @@ use tch::IndexOp;
 const USE_GPU: bool = true;
 
 include!("export.rs");
-
-fn convert_encoded_video_to_raw(
-    path_file_video_input: &str,
-    path_file_video_output: &str,
-    fps: f32,
-    size_x: u16,
-    size_y: u16,
-    size_c: u8,
-) -> anyhow::Result<()> {
-    assert!(
-        size_c == 3,
-        "Currently only implemented for 3 channel color videos..."
-    );
-
-    if std::fs::exists(path_file_video_output)? {
-        eprintln!("Not running ffmpeg, output file already exists.");
-        return Ok(());
-    }
-
-    let res = std::process::Command::new("ffmpeg")
-        .args([
-            "-i",
-            path_file_video_input,
-            "-r",
-            fps.to_string().as_str(),
-            "-nostdin",
-            "-f",
-            "rawvideo",
-            "-pix_fmt",
-            "rgb24",
-            "-vf",
-            ("scale=".to_string()
-                + size_x.to_string().as_str()
-                + ":"
-                + size_y.to_string().as_str())
-            .as_str(),
-            path_file_video_output,
-        ])
-        .status()?;
-
-    match res.code() {
-        None => {
-            return Err(anyhow::format_err!("FFmpeg failed with unknown error"));
-        }
-        Some(e) => {
-            if e == 0 {
-                return Ok(());
-            } else {
-                return Err(anyhow::format_err!("FFmpeg failed with error code {}", e));
-            }
-        }
-    };
-}
-
-fn get_file_hash(path_file_input: &str) -> anyhow::Result<u64> {
-    let file: std::fs::File = std::fs::File::open(path_file_input)?;
-    let input: memmap2::Mmap = unsafe { memmap2::Mmap::map(&file) }?;
-    Ok(gxhash::gxhash64(&input, /* seed = */ 12345))
-}
-
-fn get_str_hash(path_file_input: &str) -> u64 {
-    gxhash::gxhash64(
-        /* input = */ path_file_input.as_bytes(),
-        /* seed = */ 12345,
-    )
-}
 
 fn do_pad_video(tensor_video: &tch::Tensor) -> anyhow::Result<tch::Tensor> {
     let size: Vec<i64> = tensor_video.size();
