8d4005374f105ff0c6c3bf199bc88cbf5d72d6be
asd <aravindhv10@gmail.com>
2026-04-01 21:51:13 +0530

    Added named semaphores and some cleanups

--- video_fft/src/export.cpp
+++ video_fft/src/export.cpp
@@ -9,8 +9,3 @@ int do_fft_compress_efficient(void *const blob, uint16_t const len_t,
                               uint8_t const len_c, float32_t const fps,
                               float32_t const freq_limit, void *const dest,
                               bool use_gpu);
-
-int do_fft_compress(void *const blob, uint16_t const len_t,
-                    uint16_t const len_y, uint16_t const len_x,
-                    uint8_t const len_c, float32_t const fps,
-                    float32_t const freq_limit, void *const dest, bool use_gpu);
--- video_fft/src/export.rs
+++ video_fft/src/export.rs
@@ -17,17 +17,3 @@ unsafe extern "C" {
         use_gpu: bool,
     ) -> ::std::os::raw::c_int;
 }
-
-unsafe extern "C" {
-    pub fn do_fft_compress(
-        blob: *mut ::std::os::raw::c_void,
-        len_t: u16,
-        len_y: u16,
-        len_x: u16,
-        len_c: u8,
-        fps: float32_t,
-        freq_limit: float32_t,
-        dest: *mut ::std::os::raw::c_void,
-        use_gpu: bool,
-    ) -> ::std::os::raw::c_int;
-}
