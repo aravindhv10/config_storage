6b65483def9c06d357a3b761ed6cbc4c2e33addb
asd <aravindhv10@gmail.com>
2026-04-01 20:34:05 +0530

    Started writing semaphor codes

---
+++ semaphor_example/wait.cc
@@ -0,0 +1,11 @@
+#include <semaphore.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main (int const argc, char const ** argv) {
+  sem_t * sem = sem_open("/testing", /*int oflag =*/ O_CREAT, 0700, 2 );
+  if(sem != SEM_FAILED) {
+    printf("Successfully opened the semaphor\n");
+  }
+  sem_wait(sem);
+}
