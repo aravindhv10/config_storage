855d683655f17886e23b54a751421e24c93793e6
Your Name <you@example.com>
2026-04-03 15:31:00 +0530

    Added named semaphores and some cleanups

--- video_fft/src/export.rs
+++ video_fft/src/export.rs
@@ -5,6 +5,16 @@ pub type intype = u8;
 pub type outtype = float32_t;
 
 unsafe extern "C" {
+    pub fn new_infer_slave(batch_size: ::std::os::raw::c_uchar) -> *mut ::std::os::raw::c_void;
+    pub fn delete_infer_slave(in_: *mut ::std::os::raw::c_void);
+    pub fn run_infer_slave(
+        in_: *mut ::std::os::raw::c_void,
+        blob_source: *mut ::std::os::raw::c_void,
+        blob_destination: *mut ::std::os::raw::c_void,
+    );
+}
+
+unsafe extern "C" {
     pub fn do_fft_compress_efficient(
         blob: *mut ::std::os::raw::c_void,
         len_t: u16,
