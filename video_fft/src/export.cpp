0a0c94426541fa0a79ba0dc2be25d9ab3536c934
Your Name <you@example.com>
2026-04-03 15:28:23 +0530

    Added named semaphores and some cleanups

--- video_fft/src/export.cpp
+++ video_fft/src/export.cpp
@@ -4,6 +4,10 @@ using float64_t = double;
 using intype = unsigned char;
 using outtype = float;
 
+void *new_infer_slave(unsigned char batch_size);
+void delete_infer_slave(void *in);
+void run_infer_slave(void *in, void *blob_source, void *blob_destination);
+
 int do_fft_compress_efficient(void *const blob, uint16_t const len_t,
                               uint16_t const len_y, uint16_t const len_x,
                               uint8_t const len_c, float32_t const fps,
