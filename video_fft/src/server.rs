3c7ca7e12862f8d7a4961465365ceacc886598a2
Your Name <you@example.com>
2026-04-04 20:01:20 +0530

    Some cleanups for cargo toml

--- video_fft/src/server.rs
+++ video_fft/src/server.rs
@@ -16,6 +16,18 @@ use tch::IndexOp;
 
 const USE_GPU: bool = true;
 
+struct message_input {
+    tensor: videofft::fft_video,
+    oneshot_send_channel: oneshot::Receiver<inferencerelated::infer_results>,
+}
+
+struct inference_slave {
+    receiver: flume::Receiver<message_input>,
+}
+
 fn main() -> anyhow::Result<()> {
+    let (sender, receiver) = oneshot::channel::<inferencerelated::infer_results>();
+    let (sender, receiver) = flume::unbounded::<message_input>();
+
     Ok(())
 }
