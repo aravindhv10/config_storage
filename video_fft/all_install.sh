d577f746cec7199d04dbc28cbcff7ecc0d926b65
Your Name <you@example.com>
2026-04-06 16:56:55 +0530

    Fully working inference loop + communicator

--- video_fft/all_install.sh
+++ video_fft/all_install.sh
@@ -2,4 +2,6 @@
 cd "$(dirname -- "${0}")"
 cp -vf -- './target/libmytorch.so' './target/libmytorch.so.1.0' '/lib/'
 cp -vf -- './target/release/video_fft' '/bin/'
+cp -vf -- './target/release/infer-server' '/bin/'
+cp -vf -- './target/release/infer-client' '/bin/'
 exit '0'
