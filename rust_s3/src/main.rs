bd09d50732ecb7dea6181668c9a8ef834e7c3f45
Your Name <you@example.com>
2026-04-22 22:14:50 +0530

    More updates to s3 rust code

--- rust_s3/src/main.rs
+++ rust_s3/src/main.rs
@@ -3,6 +3,19 @@ use aws_config::BehaviorVersion;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = aws_config::load_from_env().await;
-    println!("Hello, world!");
+
+    let s3_config = aws_sdk_s3::config::Builder::from(&config)
+        .force_path_style(true)
+        .build();
+
+    let client = aws_sdk_s3::Client::from_conf(s3_config);
+    let bucket = "rdvideodataset";
+
+    let list_output = client.list_objects_v2().bucket(bucket).send().await?;
+
+    for object in list_output.contents() {
+        println!("Key: {}", object.key().unwrap_or("Unknown"));
+    }
+
     return Ok(());
 }
