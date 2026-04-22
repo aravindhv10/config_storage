a0669d114e5b3659e63c7a56667eec51e5553c7d
Your Name <you@example.com>
2026-04-22 21:21:12 +0530

    More updates to s3 code

--- rust_s3/src/main.rs
+++ rust_s3/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use aws_config::BehaviorVersion;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = aws_config::load_from_env().await;
     println!("Hello, world!");
+    return Ok(());
 }
