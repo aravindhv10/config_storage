b5a6b22cecdd11bc5514484208ea5885ead09771
Your Name <you@example.com>
2026-04-26 14:28:34 +0530

    Added a check to ensure repetedly getting the same file does not cause a race condition

--- video_fft/src/videoview.rs
+++ video_fft/src/videoview.rs
@@ -142,3 +142,122 @@ impl video_slicer {
         return Ok(tensor_data);
     }
 }
+
+pub struct video_slicer_piped {
+    fps: f32,
+
+    size_t: u16,
+    size_x: u16,
+    size_y: u16,
+    size_c: u8,
+
+    dist_t: usize,
+    dist_x: usize,
+    dist_y: usize,
+    dist_c: usize,
+
+    raw_video: Vec<u8>,
+}
+
+impl video_slicer_piped {
+    pub fn new(
+        binary_video_input: Vec<u8>
+        fps: f32,
+        size_x: u16,
+        size_y: u16,
+        size_c: u8,
+    ) -> anyhow::Result<Self> {
+
+        let raw_video = convert_encoded_video_to_raw_piped(
+            /*input_buffer: Vec<u8> =*/ binary_video_input,
+            /*fps: f32 =*/ fps,
+            /*size_x: u16 =*/ size_x,
+            /*size_y: u16 =*/ size_y,
+            /*size_c: u8 =*/ size_c,
+        ) ;
+
+        let size_t: u16 =
+            ( raw_video.len() / ((size_x as usize) * (size_y as usize) * (size_c as usize))) as u16;
+
+        let dist_c: i32 = 1;
+
+        let dist_c: usize = 1;
+        let dist_x: usize = dist_c * (size_c as usize);
+        let dist_y: usize = dist_x * (size_x as usize);
+        let dist_t: usize = dist_y * (size_y as usize);
+
+        let ret: Self = Self {
+            fps: fps,
+
+            size_t: size_t,
+            size_x: size_x,
+            size_y: size_y,
+            size_c: size_c,
+
+            dist_t: dist_t,
+            dist_x: dist_x,
+            dist_y: dist_y,
+            dist_c: dist_c,
+
+            raw_video: raw_video,
+        };
+
+        return Ok(ret);
+    }
+
+    #[inline(always)]
+    fn get_size(&self, i: u8) -> usize {
+        match i {
+            0 => self.size_c as usize,
+            1 => self.size_x as usize,
+            2 => self.size_y as usize,
+            3 => self.size_t as usize,
+            _ => 1 as usize,
+        }
+    }
+
+    #[inline(always)]
+    fn get_dist(&self, i: u8) -> usize {
+        match i {
+            0 => self.dist_c,
+            1 => self.dist_x,
+            2 => self.dist_y,
+            3 => self.dist_t,
+            _ => self.raw_video.len(),
+        }
+    }
+
+    pub fn get_video_tensor(&self) -> anyhow::Result<tch::Tensor> {
+        if self.size_t < 2 {
+            return Err(anyhow::format_err!("The video blob seems too small"));
+        }
+
+        let data: *const u8 = self.raw_video.as_ptr();
+
+        let size: [i64; 4] = [
+            self.get_size(3) as i64,
+            self.get_size(2) as i64,
+            self.get_size(1) as i64,
+            self.get_size(0) as i64,
+        ];
+
+        let strides: [i64; 4] = [
+            self.get_dist(3) as i64,
+            self.get_dist(2) as i64,
+            self.get_dist(1) as i64,
+            self.get_dist(0) as i64,
+        ];
+
+        let tensor_data: tch::Tensor = unsafe {
+            tch::Tensor::from_blob(
+                /* data = */ self.raw_video.as_slice(),
+                /* size = */ &size,
+                /* strides = */ &strides,
+                tch::Kind::Uint8,
+                tch::Device::Cpu,
+            )
+        };
+
+        return Ok(tensor_data);
+    }
+}
