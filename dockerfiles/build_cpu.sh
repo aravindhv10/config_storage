4ca0d5a89fae70b52a1db474c0bfd5a8c366c568
asd <aravindhv10@gmail.com>
2026-07-24 21:22:11 +0530

    Updated rustup install script hash

--- dockerfiles/build_cpu.sh
+++ dockerfiles/build_cpu.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
-'./build_container.sh' 'CPU/0_base'
-'./build_container.sh' 'CPU/1_install_packages'
-'./build_container.sh' 'CPU/2_install_rust'
-'./build_container.sh' 'CPU/3_good_setup'
-'./build_container.sh' 'CPU/4_python'
-'./build_container.sh' 'CPU/5_libtorch'
-'./build_container.sh' 'CPU/6_pytorch'
+'./build_container.sh' 'CPU/0_base/Dockerfile'
+'./build_container.sh' 'CPU/1_install_packages/Dockerfile'
+'./build_container.sh' 'CPU/2_install_rust/Dockerfile'
+'./build_container.sh' 'CPU/3_good_setup/Dockerfile'
+'./build_container.sh' 'CPU/4_python/Dockerfile'
+'./build_container.sh' 'CPU/5_libtorch/Dockerfile'
+'./build_container.sh' 'CPU/6_Common_python_data/Dockerfile'
+'./build_container.sh' 'CPU/7_deep_learning/Dockerfile'
 exit '0'
